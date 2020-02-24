de8472bac977114d0f134991ff25aed2d11e5855
anastasiiatsyganenko <anastasia.tsyganenko@tra.ai>
2020-02-25 00:49:55 +0100

    dao implementation draft

--- app/src/main/java/com/free/your_mind/TaskDetailsFragment.kt
+++ app/src/main/java/com/free/your_mind/TaskDetailsFragment.kt
@@ -7,15 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
-import com.free.your_mind.data.Tasks
+import com.free.your_mind.data.*
 import com.free.your_mind.databinding.TaskDetailsFragmentBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class TaskDetailsFragment : Fragment() {
+
+    private lateinit var dataSource: TaskDatabaseDao
+
+    private var viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +37,14 @@ class TaskDetailsFragment : Fragment() {
             false
         )
 
+        val application = requireNotNull(this.activity).application
+        val dataSource = TaskDatabase.getInstance(application).taskDatabaseDao
+        val viewModelFactory = DoneTaskViewModelFactory(dataSource, application)
+        val viewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(DoneTaskViewModel::class.java)
+
         val args = TaskDetailsFragmentArgs.fromBundle(arguments!!)
+
         val task = Tasks.challenges.get(args.taskId)
         if (task != null) {
             binding.name.text = task.name
@@ -36,11 +52,12 @@ class TaskDetailsFragment : Fragment() {
         }
 
         binding.doneButton.setOnClickListener { view: View ->
+            viewModel.markTaskAsDone(args.taskId)
             view.findNavController()
                 .navigate(R.id.action_taskDetailsFragment_to_congratulationFragment)
         }
+
         return binding.root
     }
-
 
 }
--- app/src/main/java/com/free/your_mind/TasksFragment.kt
+++ app/src/main/java/com/free/your_mind/TasksFragment.kt
@@ -8,7 +8,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import com.free.your_mind.data.DoneTaskViewModel
+import com.free.your_mind.data.DoneTaskViewModelFactory
+import com.free.your_mind.data.TaskDatabase
 import com.free.your_mind.data.Tasks
 import com.free.your_mind.databinding.TasksFragmentBinding
 
@@ -26,18 +31,31 @@ class TasksFragment : Fragment() {
             R.layout.tasks_fragment, container, false
         )
 
-        Tasks.challenges.forEach { (id, task) ->
-            val textView = TextView(this.context)
-            textView.textSize = 20f
-            textView.text = task.name
-            textView.setOnClickListener { view: View ->
-                view.findNavController().navigate(TasksFragmentDirections.actionTasksFragmentToTaskDetailsFragment(id))
+        val application = requireNotNull(this.activity).application
+        val dataSource = TaskDatabase.getInstance(application).taskDatabaseDao
+        val viewModelFactory = DoneTaskViewModelFactory(dataSource, application)
+        val viewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(DoneTaskViewModel::class.java)
+
+        viewModel.doneTasks.observe(this, Observer {
+            Tasks.challenges.forEach { (id, task) ->
+                val textView = TextView(this.context)
+                textView.textSize = 20f
+                textView.text =
+                    if (viewModel.doneTasks.value?.contains(id) == true) task.name.plus(" #DONE") else task.name
+                textView.setOnClickListener { view: View ->
+                    view.findNavController()
+                        .navigate(
+                            TasksFragmentDirections.actionTasksFragmentToTaskDetailsFragment(
+                                id
+                            )
+                        )
+                }
+                binding.tasks.addView(textView)
             }
-            binding.tasks.addView(textView)
-        }
+        })
 
         return binding.root
     }
-
 
 }
