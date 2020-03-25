2862631f0fd01e91309defe60389003da06f822f
anastasiiatsyganenko <anastasia.tsyganenko@tra.ai>
2020-03-25 12:04:16 +0100

    refactoring

--- app/src/main/java/com/free/your_mind/Util.kt
+++ app/src/main/java/com/free/your_mind/Util.kt
@@ -1,6 +1,22 @@
 package com.free.your_mind
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TaskViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+class TaskViewHolder private constructor(val textView: TextView) :
+    RecyclerView.ViewHolder(textView) {
+    fun bind(item: Int) {
+        textView.text = item.toString()
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): TaskViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater
+                .inflate(R.layout.text_item_view, parent, false) as TextView
+            return TaskViewHolder(view)
+        }
+    }
+}
