18fdd539472a2377675e11c647c9aa6a166d7962
anastasiiatsyganenko <anastasia.tsyganenko@tra.ai>
2020-05-01 23:45:17 +0200

    fixed issue with nav controller

--- app/src/main/java/com/free/your_mind/MainActivity.kt
+++ app/src/main/java/com/free/your_mind/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.free.your_mind
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.free.your_mind.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,27 +13,21 @@ import timber.log.Timber
 class MainActivity : AppCompatActivity() {
 
 
-
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        drawerLayout = binding.drawerLayout
-
         setSupportActionBar(toolbar)
 
         val navController = this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
         Timber.i("MainActivity created")
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.navHostFragment)
-        return NavigationUI.navigateUp(navController, drawerLayout)
+        return navController.navigateUp()
     }
-
 }
